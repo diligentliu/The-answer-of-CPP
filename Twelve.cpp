f0ce5c6c4d3aad17948a52c574126a6a89f9ac5b
moshen21 <3391569802@qq.com>
2020-05-06 09:36:16 +0800

    202005060936

--- Twelve.cpp
+++ Twelve.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include "Twelve.h"
+using namespace std;
 //12_1
 Cow::Cow() {
     strcpy(name, "");
@@ -37,8 +38,120 @@ Cow & Cow::operator = (const Cow &c) {
     return *this;
 }
 void Cow::ShowCow() const {
-    std::cout << "Name: " << name << std::endl;
-    std::cout << "Hobby: " << hobby << std::endl;
-    std::cout << "Weight: " << weight << std::endl;
+    cout << "Name: " << name << std::endl;
+    cout << "Hobby: " << hobby << std::endl;
+    cout << "Weight: " << weight << std::endl;
 }
 //12_2
+int String::num_strings = 0;
+int String::HowMany() {
+    return num_strings;
+}
+void String::stringlow() {
+    for (int i = 0; i < len; ++i) {
+        str[i] = tolower(str[i]);
+    }
+}
+void String::stringup() {
+    for (int i = 0; i < len; ++i) {
+        str[i] = toupper(str[i]);
+    }
+}
+int String::has(char c) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        if (str[i] == c)
+            ++count;
+    }
+    return count;
+}
+String::String(const char *s) {
+    len = strlen(s);
+    str = new char[len + 1];
+    strcpy(str, s);
+    ++num_strings;
+}
+String::String() {
+    len = 4;
+    str = new char[1];
+    str[0] = '\0';
+    num_strings++;
+}
+String::String(const String & st) {
+    ++num_strings;
+    len = st.len;
+    str = new char[len + 1];
+    strcpy(str, st.str);
+}
+String::~String() {
+    --num_strings;
+    delete [] str;
+}
+String & String::operator = (const String & st) {
+    if (this == &st)
+        return *this;
+    delete[] str;
+    len = st.len;
+    str = new char[len + 1];
+    strcpy(str, st.str);
+    return *this;
+}
+String & String::operator = (const char * s) {
+    delete[] str;
+    len = strlen(s);
+    str = new char[len + 1];
+    strcpy(str, s);
+    return *this;
+}
+String String::operator + (const String & st) {
+    int length = len + st.length() - 1;
+    char str2[length];
+    for (int i = 0; i < len - 1; ++i) {
+        str2[i] = str[i];
+    }
+    for (int i = len - 1; i < length; ++i) {
+        str2[i] = st.str[i];
+    }
+    return String(str2);
+}
+char & String::operator[](int i) {
+    return str[i];
+}
+const char & String::operator[](int i)const {
+    return str[i];
+}
+String operator + (const char * s, String & st){
+    int length = strlen(s) + st.length() - 1;
+    char str2[length];
+    for (int i = 0; i < strlen(s) - 1; ++i) {
+        str2[i] = s[i];
+    }
+    for (int i = 0; i < length; ++i) {
+        str2[i] = st.str[i];
+    }
+    return String(str2);
+}
+bool operator < (const String & st1, const String & st2) {
+    return (strcmp(st1.str, st2.str) < 0);
+}
+bool operator > (const String & st1, const String & st2) {
+    return st2.str < st1.str;
+}
+bool operator == (const String & st1, const String & st2) {
+    return (strcmp(st1.str, st2.str) == 0);
+}
+ostream & operator << (ostream & os, const String & st) {
+    os << st.str;
+    return os;
+}
+istream & operator >> (istream & is, String & st) {
+    char temp[String::CINLIN];
+    is.get(temp, String::CINLIN);
+    if (is)
+        st = temp;
+    while (is && is.get() != '\n')
+        continue;
+    return is;
+}
+//12_3
+
--- Twelve.h
+++ Twelve.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #ifndef STUDY_TWELVE_H
 #define STUDY_TWELVE_H
 //12_1
@@ -15,5 +16,36 @@ public:
     void ShowCow() const;
 };
 //12_2
+using std::ostream;
+using std::istream;
+class String {
+private:
+    char * str;
+    int len;
+    static int num_strings;
+    static const int CINLIN = 80;
+public:
+    String(const char * s);
+    String();
+    String(const String &);
+    ~String();
+    int length()const {return len; }
+    void stringlow();
+    void stringup();
+    int has(char c);
+    String & operator = (const String &);
+    String & operator = (const char *);
+    String operator + (const String &);
+    char & operator [] (int i);
+    const char & operator [] (int i) const;
+    friend String operator + (const char * s, String & st);
+    friend bool operator < (const String & st, const String & st2);
+    friend bool operator > (const String & st, const String & st2);
+    friend bool operator == (const String & st, const String & st2);
+    friend ostream & operator << (ostream & os, const String & st);
+    friend istream & operator >> (istream & is, String & st);
+    static int HowMany();
+};
+//12_3
 
 #endif //STUDY_TWELVE_H
