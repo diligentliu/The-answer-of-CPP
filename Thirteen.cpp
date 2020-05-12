9e9888879f0d5d2c2fc0cd95b8d16c153e256612
moshen21 <3391569802@qq.com>
2020-05-12 12:58:18 +0800

    ADD Chapter13_1

--- Thirteen.cpp
+++ Thirteen.cpp
@@ -1,2 +1,46 @@
 #include <iostream>
+#include <cstring>
 #include "Thirteen.h"
+//13_1
+Cd::Cd(char *s1, char *s2, int n, double x) {
+    strcpy(performers, s1);
+    strcpy(label, s2);
+    selections = n;
+    playtime = x;
+}
+Cd::Cd() {
+    strcpy(performers, "null");
+    strcpy(label, "null");
+    selections = 0;
+    playtime = 0.0;
+}
+Cd::~Cd() { }
+void Cd::Report() const {
+    std::cout << "Performers: " << performers << std::endl
+              << "Label: " << label << std::endl
+              << "Selections: " << selections << std::endl
+              << "Play Time: " << playtime << std::endl;
+}
+Classic::Classic(char *s1, char *s2, char *s3, char n, double x) :Cd(s1, s2, n, x) {
+    strcpy(mainworks, s3);
+}
+Classic::Classic() :Cd() {
+    strcpy(mainworks, "null");
+}
+Classic::~Classic() { }
+void Classic::Report() const {
+    Cd::Report();
+    std::cout << "Main Works: " << mainworks << std::endl;
+}
+//13_2
+
+
+
+
+
+
+
+
+
+
+
--- Thirteen.h
+++ Thirteen.h
@@ -1,8 +1,38 @@
 #ifndef STUDY_THIRTEEN_H
 #define STUDY_THIRTEEN_H
-
-class Thirteen {
-
+//13——1
+class Cd {
+private:
+    char performers[50];
+    char label[20];
+    int selections;
+    double playtime;
+public:
+    Cd(char * s1, char * s2, int n, double x);
+    Cd();
+    virtual ~Cd();
+    virtual void Report() const;
 };
+class Classic : public Cd {
+private:
+    char mainworks[50];
+public:
+    Classic(char * s1, char * s2, char * s3, char n, double x);
+    Classic();
+    virtual ~Classic();
+    virtual void Report() const;
+};
+//13_2
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //STUDY_THIRTEEN_H
