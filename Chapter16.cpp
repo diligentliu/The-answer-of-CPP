9c1eaf791e3f784172dc903f9eff6169c33d9047
moshen21 <3391569802@qq.com>
2020-06-23 22:12:58 +0800

    ADD C16E10 OVER C16

--- Chapter16.cpp
+++ Chapter16.cpp
@@ -284,8 +284,96 @@ int main() {
     return 0;
 }
 */
-/*16_10*/
+/*16_10
+struct Review {
+    string title;
+    int rating;
+    double price;
+};
+
+bool operator<(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2);
+bool worseThan(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2);
+bool priceup(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2);
+bool FillReview(shared_ptr<Review>& rr);
+void ShowReview(const shared_ptr<Review>& rr);
+
 int main() {
-    
+    vector<shared_ptr<Review>> books;
+    shared_ptr<Review> temp;
+    while (FillReview(temp)) books.push_back(temp);
+    if (books.size() > 0) {
+        cout << "Thank you. You entered the following " << books.size()
+             << " ratings:\n"
+             << "Price\tRating\tBook\n";
+        for_each(books.begin(), books.end(), ShowReview);
+
+        sort(books.begin(), books.end());
+        cout << "Sorted by title:\nPrice\tRating\tBook\n";
+        for_each(books.begin(), books.end(), ShowReview);
+
+        sort(books.begin(), books.end(), worseThan);
+        cout << "Sorted by rating up:\nPrice\tRating\tBook\n";
+        for_each(books.begin(), books.end(), ShowReview);
+
+        cout << "Sorted by rating down:\nPrice\tRating\tBook\n";
+        for_each(books.rbegin(), books.rend(), ShowReview);
+
+        sort(books.begin(), books.end(), priceup);
+        cout << "Sorted by price up:\nPrice\tRating\tBook\n";
+        for_each(books.begin(), books.end(), ShowReview);
+
+        cout << "Sorted by price down:\nPrice\tRating\tBook\n";
+        for_each(books.rbegin(), books.rend(), ShowReview);
+    } else
+        cout << "No entries. ";
+    cout << "Bye.\n";
     return 0;
 }
+
+bool operator<(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2) {
+    if (r1->title < r2->title)
+        return true;
+    else if (r1->title == r2->title && r1->rating < r2->rating)
+        return true;
+    else if (r1->title == r2->title && r1->rating == r2->rating &&
+             r1->price < r2->price)
+        return true;
+    else
+        return false;
+}
+
+bool worseThan(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2) {
+    if (r1->rating < r2->rating)
+        return true;
+    else if (r1->rating == r2->rating && r1->price < r2->price)
+        return true;
+    else
+        return false;
+}
+
+bool priceup(const shared_ptr<Review>& r1, const shared_ptr<Review>& r2) {
+    if (r1->price < r2->price)
+        return true;
+    else
+        return false;
+}
+
+bool FillReview(shared_ptr<Review>& rr) {
+    rr = shared_ptr<Review>(new Review);
+    cout << "Enter book title (quit to quit): ";
+    getline(cin, rr->title);
+    if (rr->title == "quit") return false;
+    cout << "Enter book rating: ";
+    cin >> rr->rating;
+    if (!cin) return false;
+    cout << "Enter book price: ";
+    cin >> rr->price;
+    if (!cin) return false;
+    while (cin.get() != '\n') continue;
+    return true;
+}
+
+void ShowReview(const shared_ptr<Review>& rr) {
+    cout << rr->price << "\t" << rr->rating << "\t" << rr->title << endl;
+}
+*/
