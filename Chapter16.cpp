eb773f96152e425fe0c8c8239ed996112c21a4ff
moshen21 <3391569802@qq.com>
2020-06-23 10:07:52 +0800

    ADD C16E6

--- Chapter16.cpp
+++ Chapter16.cpp
@@ -108,7 +108,96 @@ int reduce(T ar[], int n) {
     return unique(ar, ar + n) - ar;
 }
 */
-/*16_6*/
+/*16_6
+namespace C16_E6 {
+class Customer {
+private:
+    long arrive;
+    int processtime;
+
+public:
+    Customer() { arrive = processtime = 0; }
+    void set(long when);
+    long when() const { return arrive; }
+    int ptime() const { return processtime; }
+};
+void Customer::set(long when) {
+    processtime = std::rand() % 3 + 1;
+    arrive = when;
+}
+}  // namespace C16_E6
+typedef C16_E6::Customer Item;
+
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+const int MIN_PER_HR = 60;
+
+bool newcustomer2(double x);
+
+int main() {
+    srand(time(0));
+    cout << "Case Study: Bank of Heather Automatic Teller\n";
+    cout << "Enter maximum size of queue: ";
+    int qs;
+    cin >> qs;
+    queue<Item> line;
+    cout << "Enter the number of simulation hours: ";
+    int hours;
+    cin >> hours;
+    long cyclelimit = MIN_PER_HR * hours;
+    cout << "Enter the average number of customers per hour: ";
+    double perhour;
+    cin >> perhour;
+    double min_per_cust;
+    min_per_cust = MIN_PER_HR / perhour;
+    Item temp;
+    long turnaways = 0;
+    long customers = 0;
+    long served = 0;
+    long sum_line = 0;
+    int wait_time = 0;
+    long line_wait = 0;
+
+    for (int cycle = 0; cycle < cyclelimit; cycle++) {
+        if (newcustomer2(min_per_cust)) {
+            if (line.size() >= qs)
+                turnaways++;
+            else {
+                customers++;
+                temp.set(cycle);
+                line.push(temp);
+            }
+        }
+        if (wait_time <= 0 && !line.empty()) {
+            temp = line.front();
+            line.pop();
+            wait_time = temp.ptime();
+            line_wait += cycle - temp.when();
+            served++;
+        }
+        if (wait_time > 0) wait_time--;
+        sum_line += line.size();
+    }
+    if (customers > 0) {
+        cout << "customers accepted: " << customers << endl;
+        cout << "  customers served: " << served << endl;
+        cout << "         turnaways: " << turnaways << endl;
+        cout << "average queue size: ";
+        cout.precision(2);
+        cout.setf(ios_base::fixed, ios_base::floatfield);
+        cout << (double)sum_line / cyclelimit << endl;
+        cout << " average wait time: " << (double)line_wait / served
+             << " minutes\n";
+    } else
+        cout << "No customers!\n";
+    cout << "Done!\n";
+    return 0;
+}
+
+bool newcustomer2(double x) { return (rand() * x / RAND_MAX < 1); }
+*/
+/*16_7*/
 int main() {
     
     return 0;
